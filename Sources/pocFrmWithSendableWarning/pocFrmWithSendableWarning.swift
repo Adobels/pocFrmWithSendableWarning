62068747ad7ff55410b30556f7c87bb6a31d8d9e
= <=>
2024-10-23 16:52:31 +0200

    move public function inside Jojo type

--- Sources/pocFrmWithSendableWarning/pocFrmWithSendableWarning.swift
+++ Sources/pocFrmWithSendableWarning/pocFrmWithSendableWarning.swift
@@ -1,9 +1,15 @@
-public func hello(input: AnyHashable) {
-    warning(input: "") {
-        let _ : AnyHashable = input
+public class Jojo {
+
+    public var mmm: (() -> Void)?
+
+    public func hello(input: AnyHashable) {
+        warning(input: "") {
+            let _ : AnyHashable = input
+        }
+    }
+
+    public func warning(input: AnyHashable, content: @escaping @Sendable () -> Void) {
+        self.mmm = content
     }
 }
 
-public func warning(input: AnyHashable, content: @escaping @Sendable () -> Void) {
-    content()
-}
